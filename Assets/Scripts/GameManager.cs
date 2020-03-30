352f56973cab31668f18fa9799b1d9ce466188a9
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2020-03-30 23:31:28 +0000

    Some controller stuff.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager i;
+    public bool invulnerable = true;
+    public PlayerController[] players = new PlayerController[4];
+
+    void Awake()
+    {
+        i = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Destroy(players[0].gameObject);
+    }
+
+    public void AddPlayer(PlayerController player)
+    {
+        players[0] = player;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 move;
+    private Vector2 look;
+    public int id;
+    public float health;
+    public float mana;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        GameManager.i.AddPlayer(this);
+    }
+
+    void OnDestroy()
+    {
+        print("thing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Look();
+    }
+
+    public void Damage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            health = 0f;
+            if (!GameManager.i.invulnerable)
+                Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        //Death Mechanics here.
+    }
+
+    void Look() //Face player in looking direction.
+    {
+        if (look == Vector2.zero)
+            return;
+        float angle = Mathf.Atan2(look.x, look.y) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+
+    void Move()
+    {
+        rb.velocity = move * moveSpeed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
+    }
+
+    void OnLook(InputValue value)
+    {
+        look = value.Get<Vector2>();
+        look.x = -look.x;
+    }
+}
