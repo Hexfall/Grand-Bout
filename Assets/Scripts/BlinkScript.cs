24ece290a20387d5ffc103ec00300446eac80872
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2020-04-08 03:44:46 +0000

    Crown blinking and guide.

---
+++ Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkScript : MonoBehaviour
+{
+    public float rate = 6;
+    
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), Mathf.PingPong(Time.time * rate, 1));
+    }
+}
